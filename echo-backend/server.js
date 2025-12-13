658d44d6b0e8976fee618333457c37789a2b9ee6
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2025-12-13 01:11:52 -0600

    remove api key

--- echo-backend/server.js
+++ echo-backend/server.js
@@ -15,8 +15,8 @@ const BUCKET = 's3-stage-1-bucket-329857347';
 const s3Client = new S3Client({
   region: REGION,
   credentials: {
-    accessKeyId: '***REMOVED***',
-    secretAccessKey: '***REMOVED***',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
