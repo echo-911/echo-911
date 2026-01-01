71ae54da9de2d9f8c70f79289ba1b432fb1eb327
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2026-01-01 15:49:52 -0600

    add button loading states

--- echo-frontend/src/EmergencyDashboardInitial.jsx
+++ echo-frontend/src/EmergencyDashboardInitial.jsx
@@ -389,13 +389,32 @@ const EmergencyDashboard = () => {
               className={`px-4 py-2 text-sm rounded-xl text-white font-sans font-light backdrop-blur-md border border-white/30 transition ml-4` + (agentTwoData ? ' bg-white/20 hover:bg-white/30' : ' bg-white/10 cursor-not-allowed')}
               disabled={!agentTwoData}
             >
-              Find Dispatcher
+              {waitingForAgentTwoResponse ? 
+                (
+                  <div className='flex'>
+                    <svg className="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                    Preparing...
+                  </div>
+                ) : ("Find Dispatcher")
+              }
             </button>
             <button
               onClick={handleEndCall}
               className="px-4 py-2 text-sm rounded-xl bg-red-500/30 text-red-100 font-sans font-medium hover:bg-red-500/50 backdrop-blur-md border border-red-400/30 transition ml-4"
             >
-              End Call & Save Transcript
+              {waitingForAgent1Response ? 
+                (
+                  <div className='flex'>
+                    <svg className="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                    Saving...
+                  </div>
+                ) : ("End Call & Save Transcript")}
             </button>
           </div>
         </div>
